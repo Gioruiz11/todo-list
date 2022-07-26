22997b021c24e20a184f28a764a501f109aa133b
Gioruiz11 <gioruiz11@hotmail.com>
2022-07-26 11:22:17 -0400

    test

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import {showTaskModal, addTaskToDOM, test} from "./dom-change.js";
 
 // make todo modal appear on button press
 let addTask = document.getElementsByClassName("addtask")[0];
+console.log(addTask)
 addTask.addEventListener("click", showTaskModal);
 let submit = document.getElementById("Submit");
 submit.addEventListener("click", test);
